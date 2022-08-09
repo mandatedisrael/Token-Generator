c5fd06cd3d35c75a9352e3edb602cb0dbdc568a4
mandatedisrael <mandatedisrael@gmail.com>
2022-08-09 04:19:51 +0100

    frontend update

--- token_generator_frontend/src/App.js
+++ token_generator_frontend/src/App.js
@@ -8,19 +8,23 @@ function App() {
         <h1 className="header-app">Token Generator</h1>
       </header>
       <div className="non-header">
-        <div className="intro">No coding skills? No problem, this is a free token generator in 5mins!</div>
-        <div className="token-info">Provide the following informations</div>
-        <form action="">
-          <label htmlFor="tokenNameId">Token Name:</label>
-          <input type="text" id="tokenNameId" name="tokenName" /><br/>
-          <label htmlFor="tokenSymbolId">Symbol:</label>
-          <input type="text" id="tokenSymbolId" name="tokenSymbol"/><br/>
-          <label htmlFor="decimal">Decimal:</label>
-          <input type="text" id="decimal" placeholder='18' name="decimalForToken"/><br/>
-          <label htmlFor="tokenSupplyId">Token Supply:</label>
-          <input type="text" id="tokenSupplyId" name="tokenSupply"/><br/>
-          <input type="submit" value="Create Token" />
-        </form>
+        <div className="intro-container">
+          <div className="logo">x</div>
+          <div className="intro">Generate free token in 5mins!</div> 
+        </div>
+        <div className="token-info">
+          <form action="">
+            <label htmlFor="tokenNameId">Token Name:</label>
+            <input type="text" id="tokenNameId" name="tokenName" /><br/>
+            <label htmlFor="tokenSymbolId">Symbol:</label>
+            <input type="text" id="tokenSymbolId" name="tokenSymbol"/><br/>
+            <label htmlFor="decimal">Decimal:</label>
+            <input type="text" id="decimal" placeholder='18' name="decimalForToken"/><br/>
+            <label htmlFor="tokenSupplyId">Token Supply:</label>
+            <input type="text" id="tokenSupplyId" name="tokenSupply"/><br/>
+            <input type="submit" value="Create Token" />
+          </form>
+        </div>
       </div>
     </div>
   );
