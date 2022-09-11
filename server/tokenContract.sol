77811a1e56bdc61bd04b543013a928e1aa58d3d4
mandatedisrael <mandatedisrael@gmail.com>
2022-09-12 00:43:02 +0100

    update token contract sol

--- server/tokenContract.sol
+++ server/tokenContract.sol
@@ -5,20 +5,14 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract TokenTemplate is ERC20 {
     address public owner;
-    string private _name;
-    string private _symbol;
     uint8 private _decimals;
+    string tokenName;
+    string tokenSymbol;
 
     event serverStatus(string serverMessage);
 
-    constructor( string memory owner, string memory tokenName, uint8 decimal, uint256 totalSupply, string memory tokenSymbol) public payable {
-        _name = tokenName;
-        _symbol = tokenSymbol;
-        _decimals = decimal;
-
-        _mint(owner, totalSupply * 10 ** _decimals);
-
-
+    constructor (uint256 _tokenSupply, address _owner) ERC20 (tokenName,tokenSymbol){
+        _mint(_owner, _tokenSupply);
 
     }
 }
